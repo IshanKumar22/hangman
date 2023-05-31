1eea067629aff838c1494b4bc3e8d414fcf2c1ef
Ishan Kumar <kumar22ishan@gmail.com>
2023-05-31 14:39:07 +0530

    Added basic game functionality

---
+++ hangman.py
@@ -0,0 +1,49 @@
+"""
+    Module for handling the entire game, excluding GUI/TUI.
+"""
+
+from typing import Callable
+from word import random_word
+
+class Game(object):
+    """
+        The main class for the hangman game itself.
+    """
+
+    def __init__(self, func: Callable):
+        """
+            Initialize the game.
+            :param func: The function to be called when the game is over.
+        """
+
+        self.word = random_word()
+        self.turns = 6
+        self.guesses = []
+        self.gameover = func
+
+    def guess(self, letter):
+        """
+            Record a guess.
+            :param letter: The letter to be guessed.
+        """
+
+        if letter in self.word:
+            guesses += letter
+        else:
+            self.turns -= 1
+            if self.turns == 0:
+                self.gameover()
+    
+    def __str__(self) -> str:
+        """
+            Gets a string representation of the letters.
+        """
+
+        s = ""
+        for chr in self.word:
+            if chr in self.guesses:
+                s += chr
+            else:
+                s += "_"
+
+        return s
---
+++ word.py
@@ -0,0 +1,19 @@
+"""
+    Module for handling the getting of a random word.
+"""
+
+from requests import get
+from random import randint
+
+URI = "https://random-word-api.herokuapp.com/word?length="
+
+def random_word(minLen: int = 5, maxLen: int = 20) -> str:
+    """
+        The main function that gets a random word of a random length ranging from minLen to maxLen.
+        :param minLen: Minimum length
+        :param maxLen: Maximum length
+        :return: A random word
+        :rtype: str
+    """
+    res = get(URI + str(randint(minLen, maxLen)))
+    return res.json()[0]
