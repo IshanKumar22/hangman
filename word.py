47b472073ddafdb5bf40c82015f51000cd5121c4
Ishan Kumar <kumar22ishan@gmail.com>
2023-05-31 17:22:07 +0530

    Fixed error with API

--- word.py
+++ word.py
@@ -7,7 +7,7 @@ from random import randint
 
 URI = "https://random-word-api.herokuapp.com/word?length="
 
-def random_word(minLen: int = 5, maxLen: int = 20) -> str:
+def random_word(minLen: int = 5, maxLen: int = 15) -> str:
     """
         The main function that gets a random word of a random length ranging from minLen to maxLen.
         :param minLen: Minimum length
